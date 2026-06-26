096401bbba20fbc8f0ab02e8e6353d73308897d8
gassad <henrique.pavan@gmail.com>
2026-06-26 14:57:11 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
-import { PluginController } from './core/PluginController.js';
-import { DEFAULT_CONFIG } from './config/defaultConfig.js';
-import { Logger } from './utils/Logger.js';
+const app = document.getElementById('dtf-master-app');
 
-const logger = new Logger({ context: 'Bootstrap' });
-const controller = new PluginController({ defaultConfig: DEFAULT_CONFIG, logger });
-
-controller.bootstrap().catch((error) => {
-  logger.error('Failed to bootstrap DTF MASTER.', { error });
-});
+if (app) {
+  app.textContent = 'DTF MASTER Loaded';
+}
